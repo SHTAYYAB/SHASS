00587dfe7a5d7c1ad2eb361aa680bc9efebc2f45
SHTAYYAB <68806565+SHTAYYAB@users.noreply.github.com>
2020-07-26 16:15:45 +0500

    Add files via upload

---
+++ ptyhon assignment.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=30
+b=25
+c=5
+d=b+c
+if a ==d:
+    print("you are right")
+else:
+    print("you are wrong")
+
+
+# In[2]:
+
+
+a=85
+b=45
+c=35
+d=70
+e=40
+g=65
+if a == b or c==g :
+        print ("first condition is applied")
+elif b == d and c==e:
+        print ("second condition is applied")
+else:
+    ("no condition is applied")
+
+
+# In[ ]:
+
+
+a = int(input("enter your islamiat marks"))
+b = int(input("enter your chem marks"))
+c = int(input("enter your phy marks"))
+d = int(input("enter your eng marks"))
+e = int(input("enter your maths marks"))
+if a >=70 and b >=70 and c >=70 and d >=70 and e >=70:
+    print("A GRADE")
+if a >=60 and b >=60 and c >=60 and d >=60 and e >=60:
+    print("A GRADE")
+if a >=50 and b >=50 and c >=50 and d >=50 and e >=50:
+    print("A GRADE")
+if a >=40 and b >=40 and c >=40 and d >=40 and e >=40:
+    print("A GRADE")
+else:
+    print("fail")
+
+
+# In[ ]:
+
+
+def add (a,b):
+    return a + b
+def subtraction (a,b):
+    return a - b
+print("select operation.")
+print("1.add")
+print("2.subtraction")
+choice=input("enter choixe(1/2)")
+number1=float(input("enter first number:"))
+number2=float(input("enter second number:"))
+if choice=='1':
+    print (number1,number2,"=",add(number1,number2))
+elif choice=='2':
+    print (number1,number2,"=",subtraction(number1,number2))
+else :
+    print("wrong")
+
+
+# In[1]:
+
+
+print ("\t\t\tSAYLANI ONLINE ACADEMY BADGE-C")
+print("Saylani Mass IT Training")
+print("\tsaylani mass it training")
+print("saylani mass it training")
+print("\tsaylani mass it training")
+print("saylani mass it training")
+print("\tsaylani mass it training")
+print("saylani mass it training")
+print("\tsaylani mass it training")
+print("\tsaylani mass it training")
+print("saylani mass it training")
+print("\tsaylani mass it training")
+
+
+# In[2]:
+
+
+print("Saylani Mass IT Training \tSaylani Mass IT Training")
+print("Saylani Mass IT Training \tSaylani Mass IT Training")
+print("Saylani Mass IT Training \tSaylani Mass IT Training")
+print("Saylani Mass IT Training \tSaylani Mass IT Training")
+
+
+# In[3]:
+
+
+print("\tSaylani Mass IT Training \nSaylani Mass IT Training \tSaylani Mass IT Training")
+print("\tSaylani Mass IT Training \nSaylani Mass IT Training \tSaylani Mass IT Training")
+print("\tSaylani Mass IT Training \nSaylani Mass IT Training \tSaylani Mass IT Training")
+print("\tSaylani Mass IT Training \nSaylani Mass IT Training \tSaylani Mass IT Training")
+print("\tSaylani Mass IT Training \nSaylani Mass IT Training \tSaylani Mass IT Training")
+
+
+# In[4]:
+
+
+print("saylani Mass IT Training")
+print("\tsaylani Mass IT Training")
+print("\t\tsaylani Mass IT Training")
+print("\t\t\tsaylani Mass IT Training")
+print("\t\tsaylani Mass IT Training")
+print("\tsaylani Mass IT Training")
+print("saylani Mass IT Training")
+
+
+# In[ ]:
+
+
+
+
